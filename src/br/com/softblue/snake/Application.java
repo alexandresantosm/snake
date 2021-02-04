c09192ebe750a806b4d6d1b507409d9bd79f2dc8
alexandresantosm <amenezes031@gmail.com>
2021-02-04 18:42:17 -0300

    Generating the first shape on the screen

--- src/br/com/softblue/snake/Application.java
+++ src/br/com/softblue/snake/Application.java
@@ -1,9 +1,12 @@
 package br.com.softblue.snake;
 
+import br.com.softblue.snake.core.GameWindow;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		GameWindow gameWindow = new GameWindow();
 	}
 
 }
---
+++ src/br/com/softblue/snake/util/Constants.java
@@ -0,0 +1,11 @@
+package br.com.softblue.snake.util;
+
+import java.awt.Color;
+
+public class Constants {
+	
+	public static final String GAME_TITLE = "Snake Game!";
+	public static final int WINDOW_WIDTH = 400;
+	public static final int WINDOW_HEIGHT = 500;
+	public static final Color BACKGROUND_COLOR = Color.BLACK;
+}
